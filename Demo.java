6e9031f7be2ca77cc05e75a4b57c7a4c7c2c6bad
jhon_zst <jhon_zst@outlook.com>
2017-03-15 20:52:44 +0800

    modiffed

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public  class Demo{
 	public static void main(String[] args){
 
-		System.out.println("zst");
+		System.out.println("Current data");
 		
 	}
 }
--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public  class Hello{
 	public static void main(String[] args){
 
-		System.out.println("zst");
+		System.out.println("世界您好");
 		
 	}
 }
