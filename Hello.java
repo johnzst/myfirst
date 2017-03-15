bd77cdace218b5803a0e8136372490510c82407d
jhon_zst <jhon_zst@outlook.com>
2017-03-15 19:43:14 +0800

    Updata Hello.java File . Add Two lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public  class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		System.out.println("Hello World11");
+		System.out.println("Hello World22");
 	}
 }
